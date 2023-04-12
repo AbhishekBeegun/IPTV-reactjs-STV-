6e6a4ae89adabe5dd5374fd1abce4562549218ba
Dooshyant Beegun <abhishekbeegun0642@gmail.com>
2023-04-12 09:20:07 +0400

    vercel.json-added

--- src/component/Loading.jsx
+++ src/component/Loading.jsx
@@ -4,8 +4,12 @@ import Loadingani from "../assets/load.webp"
 const Loading = () => {
   return (
     <div className="bg-black fixed top-0 h-screen w-screen z-50 flex flex-col justify-center items-center">
-     <img className="animate-pulse" 
-     src={Loadingani} alt="TV-BOX" />
+     <div
+     className="font-semibold text-lg flex items-center justify-center gap-2">
+    <p
+     className="border-r-2 px-4">BEEGUN</p>
+    <span className="bg-red-600 rounded-lg px-2">TV</span>
+    </div>
     </div>
   )
 }
